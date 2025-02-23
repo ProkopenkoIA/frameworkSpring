b22fab1792aed01a7c2386e4663b75d21807e247
ProkopenkoIA <boroda5384@mail.ru>
2025-02-23 22:09:19 +0500

    Home work 11

---
+++ Lsn11HomeWork/src/main/java/com/Lsn2Ex2/excersise2/Excersise2Application.java
@@ -0,0 +1,13 @@
+package com.Lsn2Ex2.excersise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excersise2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excersise2Application.class, args);
+	}
+
+}
---
+++ Lsn11HomeWork/src/test/java/com/Lsn2Ex2/excersise2/TaskServiceTestIntegration.java
@@ -0,0 +1,47 @@
+package com.Lsn2Ex2.excersise2;
+
+import com.Lsn2Ex2.excersise2.model.Task;
+import com.Lsn2Ex2.excersise2.repository.TaskRepository;
+import com.Lsn2Ex2.excersise2.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static com.Lsn2Ex2.excersise2.model.TaskStatus.IN_PROGRESS;
+import static com.Lsn2Ex2.excersise2.model.TaskStatus.NOT_STARTED;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class TaskServiceTestIntegration {
+
+    @MockBean
+    public TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService taskService;
+
+    @Test
+    public void updateTaskTest() {
+        //pre
+        Task task1 = new Task();
+        task1.setId(1L);
+        task1.setStatus(NOT_STARTED);
+
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setStatus(IN_PROGRESS);
+
+        given(taskRepository.findById(task1.getId())).willReturn(Optional.of(task1));
+
+        // action
+        taskService.updateTaskStatus(1L, task2);
+
+        //check
+        verify(taskRepository).findById(1L);
+        verify(taskRepository).save(task1);
+    }
+}
